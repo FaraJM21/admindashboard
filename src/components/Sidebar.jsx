68c9be5392eeb6c0cc6b8b78763763a3171b5cef
FaraJM21 <farruxrihsiyev@gmail.com>
2022-11-06 15:49:38 +0500

    Sidebar && Navbar Ui finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,13 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if(activeMenu && screenSize <= 900){
+      setActiveMenu(false);
+    }
+  }
 
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
@@ -25,7 +31,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 
               flex text-xl font-extrabold 
               tracking-tighter dark:text-white text-slate-900"
@@ -36,7 +42,9 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => setActiveMenu(() => setActiveMenu(!activeMenu))}
+                onClick={() =>
+                  setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+                }
                 className="text-xl rounded-full 
                  p-3 hover:bg-light-gray
                  mt-4 block md:hidden"
@@ -56,7 +64,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
                     // style={({ isActive }) => ({
                     //   backgroundColor: isActive ? currentColor : '',
                     // })}
