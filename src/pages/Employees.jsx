91bafb2e42f6936a16e28ae481520721c0006e52
FaraJM21 <farruxrihsiyev@gmail.com>
2022-11-07 13:00:58 +0500

    details changed

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page, Toolbar } from '@syncfusion/ej2-react-grids';
+import { GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page } from '@syncfusion/ej2-react-grids';
 
 import { employeesData, employeesGrid } from '../data/dummy';
 import { Header } from '../components';
@@ -19,13 +19,13 @@ const Employees = () => {
         allowSorting
         pageSettings={{ pageCount: 5 }}
         editSettings={editing}
-        toolbar={['Search']}
+        toolbar={toolbarOptions}
       >
         <ColumnsDirective>
           {/* eslint-disable-next-line react/jsx-props-no-spreading */}
           {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
         </ColumnsDirective>
-        <Inject services={[Search, Page, Toolbar]} />
+        <Inject services={[Search, Page]} />
 
       </GridComponent>
     </div>
